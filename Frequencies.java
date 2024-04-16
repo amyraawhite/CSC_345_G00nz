a0b1ab552ff4eaf14952805321f808ccee17904e
SuhaniSuranaArizona <129905056+SuhaniSuranaArizona@users.noreply.github.com>
2024-04-15 22:20:29 -0700

    Update Frequencies.java

--- Frequencies.java
+++ Frequencies.java
@@ -1,4 +1,34 @@
 // Suhani Surana
-public class Frequencies {
-  
+public class Frequencies<T> {
+    private HashTable<T, Integer> frequencyTable;
+
+    public Frequencies() {
+        frequencyTable = new HashTable<>();
+    }
+
+    public void calculateFrequencies(T[] items) {
+        for (T item : items) {
+            KeyValuePair<T, Integer> keyValue = frequencyTable.get(item);
+            if (keyValue != null) {
+                frequencyTable.put(item, keyValue.getValue() + 1); // Update existing key's value
+            } else {
+                frequencyTable.put(item, 1); // Initialize new key with frequency 1
+            }
+        }
+    }
+
+    public void displayFrequencies() {
+        for (KeyValuePair<T, Integer> entry : frequencyTable.getHashTable()) {
+            if (entry != null) {
+                System.out.println(entry.getKey() + ": " + entry.getValue());
+            }
+        }
+    }
+
+    #public static void main(String[] args) {
+        #Frequencies<Character> frequencies = new Frequencies<>();
+        #Character[] items = {'a', 'b', 'a', 'c', 'b', 'a'};
+        #frequencies.calculateFrequencies(items);
+        #frequencies.displayFrequencies();
+    }
 }
