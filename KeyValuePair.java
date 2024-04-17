03340719625356f6554984bf559750661b7e2814
amyraawhite <148113395+amyraawhite@users.noreply.github.com>
2024-04-16 22:03:12 -0700

    Update KeyValuePair.java

--- KeyValuePair.java
+++ KeyValuePair.java
@@ -3,19 +3,23 @@ public class KeyValuePair <K, V> {
 	public int value;
 	
 	public KeyValuePair(K k, int v) {
+		// initialize key and value 
 		this.key = k; 
 		this.value = v; 
 	}
 	
 	public K getKey() {
+		// return the current key value 
 		return this.key; 
 	}
 	
 	public int getValue() {
+		// return the value associated with the given key 
 		return this.value; 
 	}
 	
 	public String toString() {
+		// return a string representation of the key-value pair 
 		return key + ": " + value; 
 	}
 }
