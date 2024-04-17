260ffcf01ba97f0989594155ba9c96657e388515
SuhaniSuranaArizona <129905056+SuhaniSuranaArizona@users.noreply.github.com>
2024-04-16 21:59:45 -0700

    Update Frequencies.java
    Added Comments

--- Frequencies.java
+++ Frequencies.java
@@ -3,18 +3,20 @@
 
 import java.util.Scanner; 
 
+// Class to calculate frequencies of characters in a string
 public class Frequencies {
 	private String input; 
-    private HashTable<Character, Integer> freqTable;
-
+    private HashTable<Character, Integer> freqTable; // Hash table to store frequencies
+    // Constructor to initialize the hash table
     public Frequencies(String input) {
         freqTable = new HashTable<>();
     }
-
+    
     public void calculateFrequencies(String input) {
     	System.out.println(input);
         int i = 0; 
         while (i < input.length()) {
+		// Put each character into the hash table
         	freqTable.put(input.charAt(i));
         	i++; 
         }  
@@ -22,10 +24,11 @@ public class Frequencies {
     }
     
     public KeyValuePair<Character, Integer>[] returnUniques(){
-    	KeyValuePair[] uniques = new KeyValuePair[freqTable.getOccupied()];  
+    	KeyValuePair[] uniques = new KeyValuePair[freqTable.getOccupied()]; 
     	int index = 0; 
     	for (KeyValuePair item: freqTable.hashMap) {
     		if (item != null) {
+			// Processing unique key-value pairs from the hash table
     			uniques[index] = item; 
     			index++; 
     		}
